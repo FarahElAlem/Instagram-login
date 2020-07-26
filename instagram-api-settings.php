37bb34af298e7c942f9bb0e0cada66e82f442229
Farah El Alem <farah.el1996@gmail.com>
2020-07-26 13:02:08 +0100

    second commit

---
+++ instagram-api-settings.php
@@ -0,0 +1,7 @@
+<?php
+
+define('INSTAGRAM_CLIENT_ID', '291296452292996'); 
+define('INSTAGRAM_CLIENT_SECRET', 'dd97361982fb4f5e505d3023b338791d'); 
+define('INSTAGRAM_REDIRECT_URI', 'https://floating-peak-73723.herokuapp.com/auth/'); 
+
+?>
